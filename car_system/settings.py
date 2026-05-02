5b7c82f9ecbef2d6384b80fcc808defb07a6a9d1
Betty <betty870715@yahoo.com.tw>
2026-05-02 15:31:34 +0800

    chore(email): configure SMTP email backend for production use

--- car_system/settings.py
+++ car_system/settings.py
@@ -135,8 +135,3 @@ CACHES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
