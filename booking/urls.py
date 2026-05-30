24ec2d26cb5c1d16492d9d2bce2942c9cddca3aa
Betty <betty870715@yahoo.com.tw>
2026-05-30 21:47:08 +0800

    feat(timezone): add session-based timezone support with middleware

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('set-timezone/', views.set_timezone),
     path('home/', views.home, name='home'),
     path('login/', views.login_page, name='login'),
     path('send_otp/', views.send_otp),
--- car_system/settings.py
+++ car_system/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'car_system.middleware.TimezoneMiddleware',
 ]
 
 ROOT_URLCONF = 'car_system.urls'
